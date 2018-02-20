7d2c8c83ab1d7b5f64d5cd99dc0a656b8107e26e
Moe Machef <mohamedahmedhefnawy@gmail.com>
2018-02-20 10:34:35 +0300

    Add files via upload

---
+++ services/passport.js
@@ -0,0 +1,55 @@
+const passport = require("passport");
+const User = require("../models/user");
+const config = require("../config");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const LocalStrategy = require("passport-local");
+
+//Create local Strategy
+const localOptions = { usernameField: "email" };
+const localLogin = new LocalStrategy(localOptions, function(
+  email,
+  password,
+  done
+) {
+  User.findOne({ email: email }, function(err, user) {
+    if (err) {
+      return done(err);
+    }
+    if (!user) {
+      return done(null, false);
+    }
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) {
+        return done(err);
+      }
+      if (!isMatch) {
+        return done(null, false);
+      }
+      return done(null, user);
+    });
+  });
+});
+//Setup options for JWT Strategy
+const jwtOptions = {
+  //Tell JwtStrategy to extract JWT from the header and identify secret to decrypt jwt
+  jwtFromRequest: ExtractJwt.fromHeader("authorization"),
+  secretOrKey: config.secret
+};
+
+//Create JWT JwtStrategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  User.findById(payload.sub, function(err, user) {
+    if (err) {
+      return done(err, false);
+    }
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+
+passport.use(jwtLogin);
+passport.use(localLogin);
